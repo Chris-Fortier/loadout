4f5efc1a753b8ac85e1a749c784dd19e355cb11d
Chris-Fortier <vendor@chrisjfortier.com>
2020-06-06 10:12:01 -0700

    make landing page move between log in and new account modes, some more work on the kit page

--- src/components/pages/Landing.js
+++ src/components/pages/Landing.js
@@ -2,131 +2,164 @@ import React from "react";
 // import landingLogo from "../../img/logo-landing.png"; // thumbs up icon
 import { Link } from "react-router-dom"; // a React element for linking
 
-export default function Landing() {
-   return (
-      <div className="background-image">
-         <div className="container-fluid landing-page">
-            <div className="row">
-               <div className="col-12 offset-sm-2 col-sm-8 col-md-6 col-lg-4 col-xl-3">
-                  <div className="">
-                     <h1 className="mt-5 text-white">Loadout</h1>
-                  </div>
-                  <div className="card">
-                     <div className="card-body">
-                        <h5 className="card-title">Log In</h5>
-                        <form className="mb-0 needs-validation" noValidate>
-                           <div className="form-group">
-                              {/* <label htmlFor="existing-username-input">
-                              Username
-                           </label> */}
-                              <input
-                                 className="form-control"
-                                 id="existing-username-input"
-                                 placeholder="username"
-                                 required
-                              />
-                              <div
-                                 className="text-danger"
-                                 id="existing-username-error"
-                              >
-                                 username error
-                              </div>
-                           </div>
-                           <div className="form-group">
-                              {/* <label htmlFor="existing-password-input">
-                              Password
-                           </label> */}
-                              <input
-                                 type="password"
-                                 className="form-control"
-                                 id="existing-password-input"
-                                 placeholder="password"
-                                 required
-                              />
-                              <div
-                                 className="text-danger"
-                                 id="existing-password-error"
-                              >
-                                 password error
-                              </div>
-                           </div>
-                           <Link
-                              className="btn btn-primary btn-block my-3"
-                              id="login-button"
-                              to="/all-kits"
-                           >
-                              Log in
-                           </Link>
-                           <Link
-                              className="btn btn-secondary btn-sm btn-block"
-                              id="new-account-button"
-                              to="/all-kits"
-                           >
-                              Make New Account
-                           </Link>
-                        </form>
-                     </div>
-                  </div>
+// export default function Landing() {
+export default class Landing extends React.Component {
+   constructor() {
+      super(); // boilerplate line that needs to be in the constructor
 
-                  <div className="card d-none">
-                     <div className="card-body">
-                        <h5 className="card-title">Make New Account</h5>
-                        <form className="mb-0 needs-validation" noValidate>
-                           <div className="form-group">
-                              {/* <label htmlFor="new-username-input">
-                              Username
-                           </label> */}
-                              <input
-                                 className="form-control"
-                                 id="new-username-input"
-                                 placeholder="make a username"
-                                 required
-                              />
-                              <div
-                                 className="text-danger"
-                                 id="new-username-error"
-                              >
-                                 username error
-                              </div>
-                           </div>
-                           <div className="form-group">
-                              {/* <label htmlFor="new-password-input">
-                              Password
-                           </label> */}
-                              <input
-                                 type="password"
-                                 className="form-control"
-                                 id="new-password-input"
-                                 placeholder="make a password"
-                                 required
-                              />
-                              <input
-                                 type="password"
-                                 className="form-control"
-                                 id="new-repeat-password-input"
-                                 placeholder="repeat your password"
-                                 required
-                              />
-                              <div
-                                 className="text-danger"
-                                 id="new-password-error"
-                              >
-                                 password error
-                              </div>
-                           </div>
-                           <Link
-                              className="btn btn-primary btn-block my-3"
-                              id="login-button"
-                              to="/all-kits"
-                           >
-                              Submit New Account
-                           </Link>
-                        </form>
+      // initialize state inside the constructor via this.state = {key: value, key: value,};
+      // set default state values for each component
+      // define a component's initial state
+      this.state = {
+         landingMode: "log-in", // set to either "log-in" or "new-account"
+      };
+   }
+
+   swithToNewAccountMode() {
+      this.setState({ landingMode: "new-account" });
+   }
+
+   swithToLogInMode() {
+      this.setState({ landingMode: "log-in" });
+   }
+
+   renderLogIn() {
+      return (
+         <div className="card">
+            <div className="card-body">
+               <h5 className="card-title">Log In</h5>
+               <form className="mb-0 needs-validation" noValidate>
+                  <div className="form-group">
+                     {/* <label htmlFor="existing-username-input">
+            Username
+         </label> */}
+                     <input
+                        className="form-control"
+                        id="existing-username-input"
+                        placeholder="username"
+                        required
+                     />
+                     <div className="text-danger" id="existing-username-error">
+                        username error
                      </div>
+                  </div>
+                  <div className="form-group">
+                     {/* <label htmlFor="existing-password-input">
+            Password
+         </label> */}
+                     <input
+                        type="password"
+                        className="form-control"
+                        id="existing-password-input"
+                        placeholder="password"
+                        required
+                     />
+                     <div className="text-danger" id="existing-password-error">
+                        password error
+                     </div>
+                  </div>
+                  <Link
+                     className="btn btn-primary btn-block my-3"
+                     id="login-button"
+                     to="/all-kits"
+                  >
+                     Log in
+                  </Link>
+                  <button
+                     className="btn btn-secondary btn-sm btn-block"
+                     id="new-account-button"
+                     onClick={() => this.swithToNewAccountMode()}
+                  >
+                     Make New Account
+                  </button>
+               </form>
+            </div>
+         </div>
+      );
+   }
+
+   renderNewAccount() {
+      return (
+         <div className="card">
+            <div className="card-body">
+               <h5 className="card-title">Make New Account</h5>
+               <form className="mb-0 needs-validation" noValidate>
+                  <div className="form-group">
+                     {/* <label htmlFor="new-username-input">
+               Username
+            </label> */}
+                     <input
+                        className="form-control"
+                        id="new-username-input"
+                        placeholder="make a username"
+                        required
+                     />
+                     <div className="text-danger" id="new-username-error">
+                        username error
+                     </div>
+                  </div>
+                  <div className="form-group">
+                     {/* <label htmlFor="new-password-input">
+               Password
+            </label> */}
+                     <input
+                        type="password"
+                        className="form-control"
+                        id="new-password-input"
+                        placeholder="make a password"
+                        required
+                     />
+                     <div className="text-danger" id="new-password-error">
+                        password doesn't meet requirements
+                     </div>
+                     <input
+                        type="password"
+                        className="form-control"
+                        id="new-repeat-password-input"
+                        placeholder="repeat your password"
+                        required
+                     />
+                     <div className="text-danger" id="new-password-error">
+                        passwords don't match
+                     </div>
+                  </div>
+                  <Link
+                     className="btn btn-primary btn-block my-3"
+                     id="login-button"
+                     to="/all-kits"
+                  >
+                     Make New Account
+                  </Link>
+                  <button
+                     className="btn btn-secondary btn-sm btn-block"
+                     id="new-account-button"
+                     onClick={() => this.swithToLogInMode()}
+                  >
+                     Already Have an Account
+                  </button>
+               </form>
+            </div>
+         </div>
+      );
+   }
+
+   render() {
+      return (
+         <div className="background-image">
+            <div className="container-fluid landing-page">
+               <div className="row">
+                  <div className="col-12 offset-sm-2 col-sm-8 col-md-6 col-lg-4 col-xl-3">
+                     <div className="">
+                        <h1 className="mt-5 text-white">Loadout</h1>
+                     </div>
+                     {/* render either the log-in or new account cards depending on landingMode */}
+                     {this.state.landingMode === "log-in" && this.renderLogIn()}
+                     {this.state.landingMode === "new-account" &&
+                        this.renderNewAccount()}
                   </div>
                </div>
             </div>
          </div>
-      </div>
-   );
+      );
+   }
 }
--- src/components/ui/Kit.js
+++ src/components/ui/Kit.js
@@ -2,6 +2,7 @@ import React from "react";
 // import utils from "../utils/utils";
 // import { convertDataType } from "../utils/helpers";
 // import { Link } from "react-router-dom"; // a React element for linking
+import orderBy from "lodash/orderBy";
 
 export default class Kit extends React.Component {
    // the thing that happens first before anything else in the class happens
@@ -11,20 +12,91 @@ export default class Kit extends React.Component {
       // set default state values for each component
       this.state = {
          expanded: false,
+         showPackedItems: true,
+         putPackedOnBottom: true,
+         // allSubItems: this.items,
+         // displayedItems: this.items,
       };
+   }
+
+   // function that can toggle any true false state property
+   // toggleState(propertyName) {
+   //    this.setState({ showPackedItems: !this.state[propertyName] });
+   //    console.log("toggle", propertyName, this.state[propertyName]);
+   // }
+
+   // update the presented list of this kit's items based on this kit's states
+   updatePresentedItems(e) {
+      const displayedItems = orderBy(this.allSubItems, "name", "asc");
+      this.setState({ displayedItems: displayedItems });
    }
 
    // renders a component for every item for a kit (give it the kit object as kitData)
    renderContainingItems(kitData) {
       console.log("amount of items", kitData.items.length);
+
+      var displayedItems = []; // initialize a new list for displayed items
+
+      displayedItems = orderBy(kitData.items, "name", "asc"); // sort the items by name
+
+      // sort the items by packed status if desired, with packed items on bottom
+      if (this.state.putPackedOnBottom) {
+         displayedItems = orderBy(displayedItems, "packed", "asc");
+      }
+
+      // hide packed items if desired
+      console.log("showPackedItems", this.state.showPackedItems);
+      if (this.state.showPackedItems === false) {
+         console.log("hiding packed items in", kitData.name);
+         displayedItems = displayedItems.filter(
+            (item) => item.packed === false
+         ); // keep only the unpacked items
+      }
+
       let output = [];
-      for (let i in kitData.items) {
+
+      // add the controls at the top of the kit
+      output.push(
+         <div className="custom-control custom-switch">
+            <input
+               type="checkbox"
+               className="custom-control-input"
+               id="show-packed-switch"
+               checked={this.state.showPackedItems}
+            />
+            <label
+               className="custom-control-label"
+               htmlFor="show-packed-switch"
+            >
+               Show Packed Items
+            </label>
+         </div>
+      );
+      if (this.state.showPackedItems) {
+         output.push(
+            <div className="custom-control custom-switch">
+               <input
+                  type="checkbox"
+                  className="custom-control-input"
+                  id="packed-on-bottom-switch"
+                  checked={this.state.putPackedOnBottom}
+               />
+               <label
+                  className="custom-control-label"
+                  htmlFor="packed-on-bottom-switch"
+               >
+                  Move Packed to Bottom of List
+               </label>
+            </div>
+         );
+      }
+      for (let i in displayedItems) {
          output.push(
             <div className="col-12">
                <div className="card">
                   <div className="card-body">
-                     {kitData.items[i].name} Packed =
-                     {String(kitData.items[i].packed)}
+                     {displayedItems[i].name} Packed =
+                     {String(displayedItems[i].packed)}
                   </div>
                </div>
             </div>
@@ -64,19 +136,22 @@ export default class Kit extends React.Component {
       // );
 
       return (
-         <div className="card">
-            <div className="card-body">
-               <div className="row">
-                  <div
-                     className="col-8 btn btn-link"
-                     onClick={() => this.toggleExpanded()}
-                  >
-                     {kitData.name}
+         <div>
+            <div className="card">
+               <div className="card-body">
+                  <div className="row">
+                     <div
+                        className="col-8 btn btn-link"
+                        onClick={() => this.toggleExpanded()}
+                     >
+                        {kitData.name}
+                     </div>
+                     <div className="col-4">
+                        {numPackedItems}/{numItems} Packed
+                     </div>
+                     {this.state.expanded &&
+                        this.renderContainingItems(kitData)}
                   </div>
-                  <div className="col-4">
-                     {numPackedItems}/{numItems} Packed
-                  </div>
-                  {this.state.expanded && this.renderContainingItems(kitData)}
                </div>
             </div>
          </div>

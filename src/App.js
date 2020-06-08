0873ea288c9afb083876ca287d2d8799e2189b95
Chris-Fortier <vendor@chrisjfortier.com>
2020-06-07 20:15:24 -0700

    delete KitList.js and references as its not used

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import React from "react"
-import "./style/master.scss"
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
-import Landing from "./components/pages/Landing"
-import AllKits from "./components/pages/AllKits"
-import KitList from "./components/pages/KitList"
-import NotFound from "./components/pages/NotFound"
+import React from "react";
+import "./style/master.scss";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Landing from "./components/pages/Landing";
+import AllKits from "./components/pages/AllKits";
+import NotFound from "./components/pages/NotFound";
 
 function App() {
-  // I think these declare different urls as differnt React components under the hood
-  return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={Landing} />
-        <Route exact path="/all-kits" component={AllKits} />
-        <Route exact path="/kit-list" component={KitList} />
-        <Route component={NotFound} />
-      </Switch>
-    </Router>
-  )
+   // I think these declare different urls as differnt React components under the hood
+   return (
+      <Router>
+         <Switch>
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/all-kits" component={AllKits} />
+            <Route component={NotFound} />
+         </Switch>
+      </Router>
+   );
 }
 
-export default App
+export default App;

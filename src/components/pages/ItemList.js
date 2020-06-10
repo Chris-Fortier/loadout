fc3290ecc08adf2ee105a8cd49a708e2263759ba
Chris-Fortier <vendor@chrisjfortier.com>
2020-06-10 10:28:31 -0700

    add item navigation icons

--- src/components/pages/ItemList.js
+++ src/components/pages/ItemList.js
@@ -88,12 +88,25 @@ export default class ItemList extends React.Component {
 
          // a used so I can make the link relative to the full path
          expander = (
-            <Link
-               to={window.location.pathname + "-" + itemData.index}
-               className={"float-right packed-counter" + expanderClassSuffix}
-            >
-               {itemData.numPackedItems} / {itemData.numItems} >
-            </Link>
+            <>
+               <Link
+                  to={window.location.pathname + "-" + itemData.index}
+                  className={"float-right packed-counter" + expanderClassSuffix}
+               >
+                  {itemData.numPackedItems} / {itemData.numItems}
+                  <svg
+                     xmlns="http://www.w3.org/2000/svg"
+                     viewBox="0 0 24 24"
+                     class="icon w-8 ml-1 icon-arrow-thin-right-circle"
+                  >
+                     <circle cx="12" cy="12" r="10" class="primary" />
+                     <path
+                        class="secondary"
+                        d="M14.59 13H7a1 1 0 0 1 0-2h7.59l-2.3-2.3a1 1 0 1 1 1.42-1.4l4 4a1 1 0 0 1 0 1.4l-4 4a1 1 0 0 1-1.42-1.4l2.3-2.3z"
+                     />
+                  </svg>
+               </Link>
+            </>
          );
       }
 
@@ -218,7 +231,23 @@ export default class ItemList extends React.Component {
                                  window.location.pathname.lastIndexOf("-")
                               )}
                            >
-                              &lt;&lt; Back to {parentName}
+                              <svg
+                                 xmlns="http://www.w3.org/2000/svg"
+                                 viewBox="0 0 24 24"
+                                 class="icon mr-1 icon-arrow-thin-left-circle"
+                              >
+                                 <circle
+                                    cx="12"
+                                    cy="12"
+                                    r="10"
+                                    class="primary"
+                                 />
+                                 <path
+                                    class="secondary"
+                                    d="M9.41 11H17a1 1 0 0 1 0 2H9.41l2.3 2.3a1 1 0 1 1-1.42 1.4l-4-4a1 1 0 0 1 0-1.4l4-4a1 1 0 0 1 1.42 1.4L9.4 11z"
+                                 />
+                              </svg>
+                              Back to {parentName}
                            </Link>
                         )}
                         <div className={pageContentClasses}>

abfbf29b345e2ddcba0f40e3c84c074412185468
Chris-Fortier <vendor@chrisjfortier.com>
2020-06-23 14:24:35 -0700

    edit some flattened data

--- src/flattened-data/loadouts.js
+++ src/flattened-data/loadouts.js
@@ -1,11 +1,11 @@
 // this table stores each loadout
 const loadouts = [
-    {
-        id: "",
-        name: "One-Night Camping Trip",
-        createdAt: , // when the loadout was created
-        modifiedAt: , // the last time something was changed (item renamed, added, deleted)
-        packedAt: , // the last time something was packed or unpacked
-        creator: "84fbbb78-b2a2-11ea-b3de-0242ac130004", // the user who created the loadout
-    }
-]
+   {
+      id: "42655170-7e10-4431-8d98-c2774f6414a4",
+      name: "One-Night Camping Trip",
+      createdAt: 1592939977877, // when the loadout was created
+      lastEditAt: 1592940077877, // the last time something was changed (item renamed, added, deleted)
+      lastPackAt: 1592940177877, // the last time something was packed or unpacked
+      creator: "84fbbb78-b2a2-11ea-b3de-0242ac130004", // the user who created the loadout
+   },
+];
--- src/flattened-data/user-loadouts.js
+++ src/flattened-data/user-loadouts.js
@@ -1,9 +1,9 @@
 // this bridge table links users to loadouts (many-to-many relationship so users can share the same loadouts)
 const userLoadouts = [
    {
-      id: "",
+      id: "5482d8fc-6e63-4d1d-a6ee-b82344507b52",
       userId: "84fbbb78-b2a2-11ea-b3de-0242ac130004", // Chris
-      loadoutId: "",
+      loadoutId: "42655170-7e10-4431-8d98-c2774f6414a4", // one-night camping trip
       canEdit: true,
       canPack: true,
       isAdmin: true, // can rename, delte, share, unshare, assign priveledges, etc.

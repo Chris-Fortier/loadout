ddf1bef14f6213c4cad34ab2541f0d9a6bdca6e2
Chris-Fortier <vendor@chrisjfortier.com>
2020-07-03 18:46:40 -0700

    set up the proxy; remove cors

--- src/components/pages/LoadoutList.js
+++ src/components/pages/LoadoutList.js
@@ -10,26 +10,6 @@ import LoadoutCard from "../ui/LoadoutCard";
 // import ItemCardEdit from "../ui/ItemCardEdit";
 import axios from "axios";
 
-// // mock data to display
-// const loadouts = [
-//    {
-//       id: "42655170-7e10-4431-8d98-c2774f6414a4",
-//       name: "One-Night Camping Trip",
-//       createdAt: 1592939977877, // when the loadout was created
-//       lastEditAt: 1592940077877, // the last time something was changed (item renamed, added, deleted)
-//       lastPackAt: 1592940177877, // the last time something was packed or unpacked
-//       creator: "84fbbb78-b2a2-11ea-b3de-0242ac130004", // the user who created the loadout
-//    },
-//    {
-//       id: "e0364b00-f7fc-469c-ab82-8de3487bcc0b",
-//       name: "Day at Punchcode",
-//       createdAt: 1592940277877, // when the loadout was created
-//       lastEditAt: 1592940377877, // the last time something was changed (item renamed, added, deleted)
-//       lastPackAt: 1592940477877, // the last time something was packed or unpacked
-//       creator: "84fbbb78-b2a2-11ea-b3de-0242ac130004", // the user who created the loadout
-//    },
-// ];
-
 class LoadoutList extends React.Component {
    constructor(props) {
       super(props); // boilerplate
@@ -41,7 +21,7 @@ class LoadoutList extends React.Component {
          //    "https://raw.githubusercontent.com/Chris-Fortier/loadout/master/src/mock-data/loadouts.json"
          // )
          .get(
-            "http://localhost:3060/api/v1/user-loadouts/?userId=84fbbb78-b2a2-11ea-b3de-0242ac130004"
+            "/api/v1/user-loadouts/?userId=84fbbb78-b2a2-11ea-b3de-0242ac130004"
          )
          .then((res) => {
             console.log("axios res", res);
@@ -54,26 +34,26 @@ class LoadoutList extends React.Component {
          .catch((error) => {
             // handle error
             console.log("axios error", error);
-            this.setState({
-               loadouts: [
-                  {
-                     id: "42655170-7e10-4431-8d98-c2774f6414a4",
-                     name: "One-Night Camping Trip dummy",
-                     createdAt: 1592939977877, // when the loadout was created
-                     lastEditAt: 1592940077877, // the last time something was changed (item renamed, added, deleted)
-                     lastPackAt: 1592940177877, // the last time something was packed or unpacked
-                     creator: "84fbbb78-b2a2-11ea-b3de-0242ac130004", // the user who created the loadout
-                  },
-                  {
-                     id: "e0364b00-f7fc-469c-ab82-8de3487bcc0b",
-                     name: "Day at Punchcode dummy",
-                     createdAt: 1592940277877, // when the loadout was created
-                     lastEditAt: 1592940377877, // the last time something was changed (item renamed, added, deleted)
-                     lastPackAt: 1592940477877, // the last time something was packed or unpacked
-                     creator: "84fbbb78-b2a2-11ea-b3de-0242ac130004", // the user who created the loadout
-                  },
-               ],
-            });
+            // this.setState({
+            //    loadouts: [
+            //       {
+            //          id: "42655170-7e10-4431-8d98-c2774f6414a4",
+            //          name: "One-Night Camping Trip dummy",
+            //          createdAt: 1592939977877, // when the loadout was created
+            //          lastEditAt: 1592940077877, // the last time something was changed (item renamed, added, deleted)
+            //          lastPackAt: 1592940177877, // the last time something was packed or unpacked
+            //          creator: "84fbbb78-b2a2-11ea-b3de-0242ac130004", // the user who created the loadout
+            //       },
+            //       {
+            //          id: "e0364b00-f7fc-469c-ab82-8de3487bcc0b",
+            //          name: "Day at Punchcode dummy",
+            //          createdAt: 1592940277877, // when the loadout was created
+            //          lastEditAt: 1592940377877, // the last time something was changed (item renamed, added, deleted)
+            //          lastPackAt: 1592940477877, // the last time something was packed or unpacked
+            //          creator: "84fbbb78-b2a2-11ea-b3de-0242ac130004", // the user who created the loadout
+            //       },
+            //    ],
+            // });
          });
 
       // set default state values
